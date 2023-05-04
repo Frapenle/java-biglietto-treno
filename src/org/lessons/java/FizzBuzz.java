aab43dd2ccd6ebe14be4d9d3e39d8df855c27374
francescotrudu <francescotrudu84@gmail.com>
2023-05-04 17:22:18 +0200

    Add user input number

--- src/org/lessons/java/FizzBuzz.java
+++ src/org/lessons/java/FizzBuzz.java
@@ -1,9 +1,15 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+
 public class FizzBuzz {
 
 	public static void main(String[] args) {
-		int number = 100;
+//		user number input
+		Scanner userNumber = new Scanner(System.in);
+		System.out.println("Insert number between 1 and 1000 ");
+		int number = userNumber.nextInt();
+		
 		for (int i = 1; i <= number; i++) {
 			if(i % 15 == 0) {
 				System.out.println("FizzBuzz");
@@ -15,5 +21,6 @@ public class FizzBuzz {
 				System.out.println(i);
 			}
 		}
+		userNumber.close();
 	}
 }
